44843299c5c0e3bc5a9c9dadcbc8f03b12368ad0
Hypxr-7 <Hypxr-7@users.noreply.github.com>
2024-11-22 11:15:57 +0500

    Add print_table() function in Interpolation/utility.py

--- Interpolation/utility.py
+++ Interpolation/utility.py
@@ -4,15 +4,23 @@ def fact(num):
 
     result = num
     while num != 1:
-        result *= num-1
+        result *= num - 1
         num -= 1
     return result
+
 
 def construct_table(y):
     fd = [y]
     for i in range(1, len(y)):
         temp = []
-        for j in range(0, len(y)-i):
-            temp.append(fd[i-1][j+1] - fd[i-1][j])
+        for j in range(0, len(y) - i):
+            temp.append(fd[i - 1][j + 1] - fd[i - 1][j])
         fd.append(temp)
     return fd
+
+
+def print_table(fdd: list[list]) -> None:
+    for i in range(len(fdd)):
+        for j in range(len(fdd[i])):
+            print(f"{fdd[j][i]: <6.3f}", end=' ')
+        print()
